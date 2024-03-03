f2164884425963a90c96392a6dbee345540b966b
Viral Vaghela <viralvaghela8577@gmail.com>
2024-03-03 16:54:59 +0530

    mineflayer

---
+++ index.js
@@ -0,0 +1,36 @@
+const mineflayer = require('mineflayer');
+
+const botArgs = {
+    host:"PDEU_OfficialSmp.aternos.me",
+    port:'55222',
+    username: 'pdeu_bot',
+    version:'1.20.3'
+};
+
+const initBot = () => {
+
+    let bot = mineflayer.createBot(botArgs);
+
+    bot.on('login', () => {
+        let botSocket = bot._client.socket;
+        console.log(`Logged in to ${botSocket.server ? botSocket.server : botSocket._host}`);
+    });
+
+    bot.on('end', () => {
+        console.log(`Disconnected`);
+
+        // Attempt to reconnect
+        setTimeout(initBot, 5000);
+    });
+
+    bot.on('error', (err) => {
+        if (err.code === 'ECONNREFUSED') {
+            console.log(`Failed to connect to ${err.address}:${err.port}`)
+        }
+        else {
+            console.log(`Unhandled error: ${err}`);
+        }
+    });
+};
+
+initBot();
