0fbe31bd3e113c2d32aa7c19a2dfb49a7a8f158f
Viral Vaghela <viralvaghela8577@gmail.com>
2024-03-03 17:21:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ const initBot = () => {
     });
 };
 
-const PORT = process.env.PORT || 3000; // Use the provided port or default to 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    initBot(); // Initialize bot after the server starts listening
+    initBot();
 });
